fd26aa62cbc65e4a590fc311a87a9030e16f0a0f
Joseph Akayesi <josephakayesi@gmail.com>
2019-12-31 19:08:28 +0000

    Add proxy

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import AddBook from './components/AddBook'
 
 // Apollo client setup
 const client = new ApolloClient({
-    uri: 'http://localhost:5000/graphql'
+    uri: '/graphql'
 })
 
 function App() {
